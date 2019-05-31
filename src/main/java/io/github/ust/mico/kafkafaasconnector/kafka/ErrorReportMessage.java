37b8c7b50090aeb9ff0ee3bee23eb6f6711b80b5
wagnerdk <18245675+wagnerdk@users.noreply.github.com>
2019-05-31 16:48:40 +0200

    Add error report message for invalid and dead letter topic

---
+++ src/main/java/io/github/ust/mico/kafkafaasconnector/kafka/ErrorReportMessage.java
@@ -0,0 +1,20 @@
+package io.github.ust.mico.kafkafaasconnector.kafka;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain = true)
+public class ErrorReportMessage {
+
+    String message;
+    String kafkaInputTopic;
+    String kafkaOutputTopic;
+    String OpenfaasGateway;
+    String OpenfaasFunctionName;
+    String kafkaFaasConnectorName;
+}
