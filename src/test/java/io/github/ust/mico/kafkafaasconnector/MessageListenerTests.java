7240da0388a594e8fef841f53defb1665937fe20
Fabian Bühler <17296905+buehlefs@users.noreply.github.com>
2019-06-03 22:47:28 +0200

    Fix some errors in unit tests
    Unit test still does not work with embedded broker
    Added test constants

--- src/test/java/io/github/ust/mico/kafkafaasconnector/MessageListenerTests.java
+++ src/test/java/io/github/ust/mico/kafkafaasconnector/MessageListenerTests.java
@@ -11,36 +11,46 @@ import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.kafka.test.rule.EmbeddedKafkaRule;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import io.github.ust.mico.kafkafaasconnector.kafka.MicoCloudEventImpl;
+import io.github.ust.mico.kafkafaasconnector.TestConstants;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @EnableAutoConfiguration
 @AutoConfigureMockMvc
-@ActiveProfiles("dev")
+@ActiveProfiles("testing")
+@DirtiesContext
 public class MessageListenerTests {
 
     // TODO explore embedded broker for running tests
     // https://docs.spring.io/spring-kafka/docs/2.2.6.RELEASE/reference/html/#kafka-testing-junit4-class-rule
     @ClassRule
-    public static EmbeddedKafkaRule broker = new EmbeddedKafkaRule(1, false, "${kafka.input-topic}");
+    public static EmbeddedKafkaRule broker = new EmbeddedKafkaRule(1, false,
+            TestConstants.INPUT_TOPIC, TestConstants.OUTPUT_TOPIC,
+            TestConstants.DEAD_LETTER_TOPIC, TestConstants.INVALID_MESSAGE_TOPIC,
+            TestConstants.ROUTING_TOPIC_1, TestConstants.ROUTING_TOPIC_2,
+            TestConstants.ROUTING_TOPIC_3, TestConstants.ROUTING_TOPIC_4);
 
     // TODO explore why autowired fails here
     private MessageListener listener = new MessageListener();
 
     @BeforeClass
     public static void setup() {
-        System.setProperty("spring.kafka.bootstrap-servers",
-                    broker.getEmbeddedKafka().getBrokersAsString());
+        String broker = System.getProperty("spring.embedded.kafka.brokers");
+        System.setProperty("spring.kafka.bootstrap-servers", broker);
+        System.getProperty("kafka.bootstrap-servers", broker);
+        //System.setProperty("spring.kafka.bootstrap-servers",
+        //            broker.getEmbeddedKafka().getBrokersAsString());
     }
-
 
     @Test
     public void parseFunctionResult() {
---
+++ src/test/java/io/github/ust/mico/kafkafaasconnector/TestConstants.java
@@ -0,0 +1,16 @@
+package io.github.ust.mico.kafkafaasconnector;
+
+public class TestConstants {
+
+    // Kafka topics
+    public static String INPUT_TOPIC = "input";
+    public static String OUTPUT_TOPIC = "output";
+    public static String INVALID_MESSAGE_TOPIC = "InvalidMessage";
+    public static String DEAD_LETTER_TOPIC = "DeadLetter";
+    public static String ROUTING_TOPIC_1 = "route-1";
+    public static String ROUTING_TOPIC_2 = "route-2";
+    public static String ROUTING_TOPIC_3 = "route-3";
+    public static String ROUTING_TOPIC_4 = "route-4";
+
+    // CloudEvents
+}
