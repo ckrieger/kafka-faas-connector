4fa7550b4d095981741555b50feb5ecf0374e44f
Pascal Weiß <pa-weiss@gmx.de>
2019-07-05 08:29:52 +0200

    Removed unnecessary PostConstruct from test

--- src/test/java/io/github/ust/mico/kafkafaasconnector/ConfigurationTests.java
+++ src/test/java/io/github/ust/mico/kafkafaasconnector/ConfigurationTests.java
@@ -19,29 +19,17 @@
 
 package io.github.ust.mico.kafkafaasconnector;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import io.github.ust.mico.kafkafaasconnector.configuration.KafkaConfig;
-import io.github.ust.mico.kafkafaasconnector.kafka.MicoCloudEventImpl;
 import org.junit.*;
 import org.junit.contrib.java.lang.system.EnvironmentVariables;
 import io.github.ust.mico.kafkafaasconnector.configuration.OpenFaaSConfig;
-import lombok.extern.slf4j.Slf4j;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.test.EmbeddedKafkaBroker;
 import org.springframework.kafka.test.rule.EmbeddedKafkaRule;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import javax.annotation.PostConstruct;
-
-import java.util.ArrayList;
-import java.util.Set;
 
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
@@ -50,10 +38,7 @@ import static org.junit.Assert.*;
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @EnableAutoConfiguration
-@AutoConfigureMockMvc
 @ActiveProfiles("testing")
-@DirtiesContext
-@Slf4j
 public class ConfigurationTests {
 
     @Autowired
@@ -62,14 +47,9 @@ public class ConfigurationTests {
     @Autowired
     private KafkaConfig kafkaConfig;
 
-    private final EmbeddedKafkaBroker embeddedKafka = broker.getEmbeddedKafka();
-
-    //     https://docs.spring.io/spring-kafka/docs/2.2.6.RELEASE/reference/html/#kafka-testing-junit4-class-rule
+    // https://docs.spring.io/spring-kafka/docs/2.2.6.RELEASE/reference/html/#kafka-testing-junit4-class-rule
     @ClassRule
     public static EmbeddedKafkaRule broker = new EmbeddedKafkaRule(1, false);
-
-    private KafkaTemplate<String, MicoCloudEventImpl<JsonNode>> template;
-    private MicoKafkaTestHelper micoKafkaTestHelper;
 
     @ClassRule
     public static final EnvironmentVariables environmentVariables = new EnvironmentVariables()
@@ -81,19 +61,6 @@ public class ConfigurationTests {
         .set("KAFKA_TOPIC_OUTPUT", "kafka_out")
         .set("KAFKA_TOPIC_DEAD_LETTER", "kafka_dead_letter")
         .set("KAFKA_TOPIC_TEST_MESSAGE_OUTPUT", "kafka_test_msg");
-
-    @PostConstruct
-    public void before(){
-        this.micoKafkaTestHelper = new MicoKafkaTestHelper(embeddedKafka,kafkaConfig);
-        template = this.micoKafkaTestHelper.getTemplate();
-        //We need to add them outside of the rule because the autowired kakfaConfig is not accessible from the static rule
-        //We can not use @BeforeClass which is only executed once because it has to be static and we do not have access to the autowired kakfaConfig
-
-        Set<String> requiredTopics = this.micoKafkaTestHelper.getRequiredTopics();
-        Set<String> alreadySetTopics = this.micoKafkaTestHelper.requestActuallySetTopics();
-        requiredTopics.removeAll(alreadySetTopics);
-        requiredTopics.forEach(topic -> embeddedKafka.addTopics(topic));
-    }
 
     @Test
     public void testOpenFaaSConfig() {
